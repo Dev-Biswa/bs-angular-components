96c9fb9528f92f25e8b43b434464c5033e642057
Biswajeet-Sahoo <biswajeetsahoo@rarecrew.com>
2025-09-08 10:56:50 +0530

    DEVELOPMENT: refactored grid logic

--- src/app/modules/custom/custom-grid/custom-grid.component.ts
+++ src/app/modules/custom/custom-grid/custom-grid.component.ts
@@ -1,5 +1,44 @@
 import { Component } from "@angular/core";
-
+import { MatTableDataSource } from "@angular/material/table";
+export interface UserData {
+    id: string;
+    name: string;
+    progress: string;
+    fruit: string;
+  }
+  
+  /** Constants used to fill up our data base. */
+const FRUITS: string[] = [
+    'blueberry',
+    'lychee',
+    'kiwi',
+    'mango',
+    'peach',
+    'lime',
+    'pomegranate',
+    'pineapple',
+];
+const NAMES: string[] = [
+    'Maia',
+    'Asher',
+    'Olivia',
+    'Atticus',
+    'Amelia',
+    'Jack',
+    'Charlotte',
+    'Theodore',
+    'Isla',
+    'Oliver',
+    'Isabella',
+    'Jasper',
+    'Cora',
+    'Levi',
+    'Violet',
+    'Arthur',
+    'Mia',
+    'Thomas',
+    'Elizabeth',
+];
 @Component({
     selector: 'app-custom-grid',
     templateUrl: './custom-grid.component.html',
@@ -7,5 +46,30 @@ import { Component } from "@angular/core";
 })
 
 export class CustomGridComponent {
+    constructor() {
+        // Create 100 users
+        const users = Array.from({length: 100}, (_, k) => createNewUser(k + 1));
+    
+        // Assign the data to the data source for the table to render
+        this.dataSource = new MatTableDataSource(users);
+    }
 
+    dataSource: MatTableDataSource<UserData>;
+    displayedColumns: string[] = ['id', 'name', 'progress', 'fruit'];
+}
+
+/** Builds and returns a new User. */
+function createNewUser(id: number): UserData {
+    const name =
+      NAMES[Math.round(Math.random() * (NAMES.length - 1))] +
+      ' ' +
+      NAMES[Math.round(Math.random() * (NAMES.length - 1))].charAt(0) +
+      '.';
+  
+    return {
+      id: id.toString(),
+      name: name,
+      progress: Math.round(Math.random() * 100).toString(),
+      fruit: FRUITS[Math.round(Math.random() * (FRUITS.length - 1))],
+    };
 }
